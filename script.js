2e88df435e5850df55ec6b50a09506f4e6e04cf6
Thab09 <ibraheem.thab@gmail.com>
2021-10-23 21:02:17 +0500

    add form to the modal

---
+++ script.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+let myLibrary = [];
+
+function addBookToLibrary() {}
