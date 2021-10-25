bf60491200a7a51f6549aea8f80ab8e726f92633
Thab09 <ibraheem.thab@gmail.com>
2021-10-25 23:21:27 +0500

    gets the user input from modal and stores it in an array

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const myModal = new bootstrap.Modal(document.getElementById("book-modal"), {
+  keyboard: true,
+});
+
+document
+  .getElementById("submit-button")
+  .addEventListener("click", addBookToLibrary);
+
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
@@ -7,4 +15,23 @@ function Book(title, author, pages, status) {
 
 let myLibrary = [];
 
-function addBookToLibrary() {}
+function addBookToLibrary(e) {
+  e.preventDefault();
+  myModal.hide();
+
+  let title = document.getElementById("book-name").value;
+  let author = document.getElementById("author-name").value;
+  let pages = document.getElementById("no-of-pages").value;
+  let status = document.querySelector(
+    'input[name="inlineRadioOptions"]:checked'
+  ).value;
+
+  let book = new Book(title, author, pages, status);
+
+  myLibrary.push(book);
+
+  document.querySelector("form").reset();
+
+  let pre = document.getElementById("pre");
+  pre.textContent = "\n" + JSON.stringify(myLibrary, "\t", 2);
+}
