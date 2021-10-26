e264ed92272d13d1e97c3542af32b502fcad6592
Thab09 <ibraheem.thab@gmail.com>
2021-10-26 22:16:24 +0500

    made cards and added some styling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const myModal = new bootstrap.Modal(document.getElementById("book-modal"), {
   keyboard: true,
 });
+const displayBooks = document.getElementById("display-books");
 
 document
   .getElementById("submit-button")
@@ -13,7 +14,16 @@ function Book(title, author, pages, status) {
   this.status = status;
 }
 
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    pages: 23,
+    status: "Reading",
+  },
+];
+
+displayLibrary();
 
 function addBookToLibrary(e) {
   e.preventDefault();
@@ -32,6 +42,50 @@ function addBookToLibrary(e) {
 
   document.querySelector("form").reset();
 
-  let pre = document.getElementById("pre");
-  pre.textContent = "\n" + JSON.stringify(myLibrary, "\t", 2);
+  localStorage.setItem("Library", JSON.stringify(myLibrary));
+
+  displayLibrary();
+}
+
+function displayLibrary() {
+  //get items from local
+  displayBooks.innerHTML = "";
+  for (const book of myLibrary) {
+    bookCardsForLibrary(book.title, book.author, book.pages, book.status);
+  }
+}
+
+function bookCardsForLibrary(title, author, pages, status) {
+  const cardContainer = document.createElement("div");
+  const buttonContainer = document.createElement("div");
+  const contentContainer = document.createElement("div");
+  const bookTitle = document.createElement("p");
+  const authorTitle = document.createElement("p");
+  const pageCount = document.createElement("p");
+  const bookStatus = document.createElement("p");
+  const editButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+
+  cardContainer.classList.add("card");
+  buttonContainer.classList.add("card-button");
+  contentContainer.classList.add("card-content");
+  bookTitle.textContent = title;
+  authorTitle.textContent = author;
+  pageCount.textContent = pages + " pages";
+  bookStatus.textContent = status;
+  editButton.innerHTML = `<i class="bi bi-pen"></i>`;
+  deleteButton.innerHTML = `<i class="bi bi-x-lg"></i>`;
+
+  editButton.onclick = () => console.log("yoyoy");
+
+  buttonContainer.appendChild(editButton);
+  buttonContainer.appendChild(deleteButton);
+  cardContainer.appendChild(buttonContainer);
+  contentContainer.appendChild(bookTitle);
+  contentContainer.appendChild(authorTitle);
+  contentContainer.appendChild(pageCount);
+  contentContainer.appendChild(bookStatus);
+  cardContainer.appendChild(contentContainer);
+
+  displayBooks.appendChild(cardContainer);
 }
