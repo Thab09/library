dfd14f8a9e8ce17cd136743dddda2adff534adfa
Thab09 <ibraheem.thab@gmail.com>
2021-10-27 23:01:40 +0500

    finished edit button and local storage

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ document
   .getElementById("submit-button")
   .addEventListener("click", addBookToLibrary);
 
-function Book(title, author, pages, status) {
+function Book(id, title, author, pages, status) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -16,6 +17,7 @@ function Book(title, author, pages, status) {
 
 let myLibrary = [
   {
+    id: 1635355542910,
     title: "Harry Potter",
     author: "J.K. Rowling",
     pages: 23,
@@ -23,12 +25,25 @@ let myLibrary = [
   },
 ];
 
+localeStorageToLibrary();
 displayLibrary();
+
+function localeStorageToLibrary() {
+  localStorage.getItem("Library") === null
+    ? false
+    : (myLibrary = JSON.parse(localStorage.getItem("Library")));
+}
+
+function LibrarytoLocaleStorage() {
+  localStorage.setItem("Library", JSON.stringify(myLibrary));
+  displayLibrary();
+}
 
 function addBookToLibrary(e) {
   e.preventDefault();
   myModal.hide();
 
+  let id = Date.now();
   let title = document.getElementById("book-name").value;
   let author = document.getElementById("author-name").value;
   let pages = document.getElementById("no-of-pages").value;
@@ -36,33 +51,36 @@ function addBookToLibrary(e) {
     'input[name="inlineRadioOptions"]:checked'
   ).value;
 
-  let book = new Book(title, author, pages, status);
+  let book = new Book(id, title, author, pages, status);
 
   myLibrary.push(book);
 
   document.querySelector("form").reset();
 
-  localStorage.setItem("Library", JSON.stringify(myLibrary));
-
-  displayLibrary();
+  LibrarytoLocaleStorage();
 }
 
 function displayLibrary() {
-  //get items from local
   displayBooks.innerHTML = "";
   for (const book of myLibrary) {
-    bookCardsForLibrary(book.title, book.author, book.pages, book.status);
+    bookCardsForLibrary(
+      book.id,
+      book.title,
+      book.author,
+      book.pages,
+      book.status
+    );
   }
 }
 
-function bookCardsForLibrary(title, author, pages, status) {
+function bookCardsForLibrary(id, title, author, pages, status) {
   const cardContainer = document.createElement("div");
   const buttonContainer = document.createElement("div");
   const contentContainer = document.createElement("div");
   const bookTitle = document.createElement("p");
   const authorTitle = document.createElement("p");
   const pageCount = document.createElement("p");
-  const bookStatus = document.createElement("p");
+  // const bookStatus = document.createElement("p");
   const editButton = document.createElement("button");
   const deleteButton = document.createElement("button");
 
@@ -72,11 +90,12 @@ function bookCardsForLibrary(title, author, pages, status) {
   bookTitle.textContent = title;
   authorTitle.textContent = author;
   pageCount.textContent = pages + " pages";
-  bookStatus.textContent = status;
+  // bookStatus.textContent = status;
   editButton.innerHTML = `<i class="bi bi-pen"></i>`;
   deleteButton.innerHTML = `<i class="bi bi-x-lg"></i>`;
 
-  editButton.onclick = () => console.log("yoyoy");
+  editButton.onclick = (e) => console.log(id);
+  deleteButton.onclick = (e) => deleteBook(id);
 
   buttonContainer.appendChild(editButton);
   buttonContainer.appendChild(deleteButton);
@@ -84,8 +103,19 @@ function bookCardsForLibrary(title, author, pages, status) {
   contentContainer.appendChild(bookTitle);
   contentContainer.appendChild(authorTitle);
   contentContainer.appendChild(pageCount);
-  contentContainer.appendChild(bookStatus);
+  // contentContainer.appendChild(bookStatus);
   cardContainer.appendChild(contentContainer);
 
   displayBooks.appendChild(cardContainer);
 }
+
+function deleteBook(id) {
+  const book = myLibrary.findIndex((book) => book.id === id);
+  myLibrary.splice(book, 1);
+  LibrarytoLocaleStorage();
+}
+
+// ADD EDIT
+// SHOW THE STATUS
+// NAV BAR - ALL / REA / YET / FINISHED
+// CSS
